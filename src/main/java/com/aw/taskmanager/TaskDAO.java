fbef218387499e6bf30c956e79addd84f9476620
AdamW <awieleba3@gmail.com>
2026-04-13 23:39:50 +0200

    małe zmiany związane z DAO

--- src/main/java/com/aw/taskmanager/TaskDAO.java
+++ src/main/java/com/aw/taskmanager/TaskDAO.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface TaskDAO {
 
-    public void saveAll(List<Task> list) throws Exception;
+    public void saveAll(List<Task> list);
 
-    public List<Task> loadAll() throws Exception;
+    public List<Task> loadAll();
 
-    public void delete(Task task) throws Exception;
+    public void delete(Task task);
 }
--- src/main/java/com/aw/taskmanager/XmlDAO.java
+++ src/main/java/com/aw/taskmanager/XmlDAO.java
@@ -16,30 +16,35 @@ public class XmlDAO implements TaskDAO {
     }
 
     @Override
-    public void saveAll(List<Task> list) throws Exception {
+    public void saveAll(List<Task> list) {
         TaskList wrapper = new TaskList();
         wrapper.setTasks(list);
 
-        JAXBContext context = JAXBContext.newInstance(TaskList.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-        marshaller.marshal(wrapper, new File(file));
+        try {
+            JAXBContext context = JAXBContext.newInstance(TaskList.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(wrapper, new File(file));       
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public List<Task> loadAll() throws Exception {
-        JAXBContext context = JAXBContext.newInstance(TaskList.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-
-        TaskList wrapper =
-                (TaskList) unmarshaller.unmarshal(new File(file));
-
-        return wrapper.getTasks();
+    public List<Task> loadAll() {
+        try {
+            JAXBContext context = JAXBContext.newInstance(TaskList.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            TaskList wrapper = (TaskList) unmarshaller.unmarshal(new File(file));
+            return wrapper.getTasks();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
     }
 
     @Override
-    public void delete(Task task) throws Exception {
+    public void delete(Task task) {
         List<Task> list = loadAll();
         list.remove(task);
         saveAll(list);
